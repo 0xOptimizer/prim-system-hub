f1d47afe7e814d279bfcb591544a6d8072697457
ビビ <38562978+0xOptimizer@users.noreply.github.com>
2025-01-12 19:36:46 +0800

    update: registration email polish

--- resources/views/emails/registration-success.blade.php
+++ resources/views/emails/registration-success.blade.php
@@ -8,8 +8,6 @@
 <body>
     <h1>Welcome, {{ $user->first_name }}!</h1>
     <p style="font-size: 16px;">Your registration as a <b>{{ $user->user_type }}</b> was successful. You can now log in to your account.</p>
-    <a href="#" target="_blank" style="border-radius: 16px; padding-top: 8px; padding-bottom: 8px; padding-left: 16px; padding-right: 16px; background-color: black; color: white; margin-top: 8px; font-size: 28px; text-decoration: none; position; relative;">
-        OPEN APP
-    </a>
+    <a href="#" target="_blank" style="border-radius: 16px; padding-top: 8px; padding-bottom: 8px; padding-left: 16px; padding-right: 16px; background-color: black; color: white; margin-top: 8px; font-size: 28px; text-decoration: none;">OPEN APP</a>
 </body>
 </html>
