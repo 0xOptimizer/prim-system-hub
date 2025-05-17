b4c6199a96e9cf37df2e87f5d4db9bb0f09d7cae
ビビ <38562978+0xOptimizer@users.noreply.github.com>
2025-05-17 23:05:13 +0800

    update: cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,9 +14,8 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -42,7 +41,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\HandlePreflight::class,
+            // \App\Http\Middleware\HandlePreflight::class,
         ],
     ];
 
