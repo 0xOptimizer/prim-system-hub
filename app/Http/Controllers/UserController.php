3f7e19ffe45b618b74a6c4e34d1649b0b943e7b6
ビビ <38562978+0xOptimizer@users.noreply.github.com>
2025-05-18 22:46:09 +0800

    hotfix: dependencies

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\RegistrationSuccess;
